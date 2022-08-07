02a85b5100477be067a1d2acdb07b4343e4971ab
pfulcher26 <pfulcher26@gmail.com>
2022-08-07 13:26:18 -0700

    Updates the foreach loop to a for loop.  Adds a previousIndex variable.  If the inner HTML of the element clicked is not equal to an empty string, then the lineHTML variable is updated with the innerHTML of the current element.  The previous index variable is updated to store the current element.  If an element is clicked and the innerHTML of the current element is not equal to the value stored in lineHTML, the innerHMTL of the previosu index is updated with the innerHTML of the current element and the current element is updatd with the innerHTML of the value stored in lineHTML. This allows users to click on a list  element, click on another list  element, and have them swap places.

--- js/main.js
+++ js/main.js
@@ -5,10 +5,54 @@ arrowIcon.addEventListener('click', ()=> {
   instructions.style.visibility !== "hidden" ? instructions.style.visibility = "hidden" : instructions.style.visibility = "visible"; 
 });
 
+//create new variable that stores current line value
+//need to establish a previous index variable to allow innerHTML content to switch
+//places
+let lineHTML= ""
+let previousIndex = ""
+
+//loops through every element in the selected group and attaches event listeners
 let line = document.querySelectorAll('.list-group-item');
-line.forEach((el) => {
-  el.addEventListener('click', function(){
-    !el.classList.contains('active') ? el.classList.add('active'): el.classList.remove('active');  
-  })
-});
+
+for(i = 0; i < line.length; i++) {
+  let currentElement = line[i] 
+  line[i].addEventListener('click', function(){
+    if (lineHTML === "") {
+      lineHTML = currentElement.innerHTML;
+      previousIndex = currentElement
+    } else {
+      lineHTML !== currentElement.innerHTML 
+      previousIndex.innerHTML = currentElement.innerHTML 
+      currentElement.innerHTML = lineHTML;
+      lineHTML = ""
+    } 
+  });
+}
+// currentElement.classList.add('disabled')
+// previousIndex.classList.remove('disabled')
+
+
+
+// line.forEach((el, index) => {
+//   el.addEventListener('click', function(){
+//     if (lineHTML === "") {
+//       lineHTML = el.innerHTML;
+//       previousIndex = el[index]
+//     } else {
+//       lineHTML !== el.innerHTML 
+//       el.innerHTML = lineHTML;
+//       previousIndex = el.innerHTML 
+//       lineHTML = ""
+//     } 
+//     // !el.classList.contains('active') ? el.classList.add('active'): el.classList.remove('active');  
+//   })
+// });
+
+
+
+
+//establish a way to select the content of the line 
+// console.log(line[0].innerHTML)
+
+
 
