eee7466953651e8f154f26b5a5911347d42457c4
pfulcher26 <pfulcher26@gmail.com>
2022-08-07 15:06:46 -0700

    Organizes javascript into state variables, cached variables, event listeners and functions.  Also adds a song that plays upon the user clicking the arrow icon and initiating the game.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-let arrowIcon = document.querySelector('#arrow-icon');
+
+//state variables 
+const clickSound = new Audio('click.wav')
+const clickSoundTwo = new Audio('click2.wav')
+const feetSong = new Audio('FEET.mp4')
+const instructions = document.querySelector('.instructions');
+const haikuLines = document.querySelector('.haiku-lines')
+const arrowIcon = document.querySelector('#arrow-icon');
 console.log(arrowIcon);
+
+//cached variables
+//create new variable that stores current line value
+let lineHTML= ""
+//need to establish a previous index variable to allow innerHTML content to switch
+//places
+let previousIndex = ""
+let line = document.querySelectorAll('.list-group-item');
+
+//event listeners 
 arrowIcon.addEventListener('click', ()=> {
-  let instructions = document.querySelector('.instructions');
-  let haikuLines = document.querySelector('.haiku-lines')
+  feetSong.play()
   haikuLines.style.visibility ="visible";
   instructions.style.visibility !== "hidden" ? instructions.style.visibility = "hidden" : instructions.style.visibility = "visible"; 
 });
 
-//create new variable that stores current line value
-//need to establish a previous index variable to allow innerHTML content to switch
-//places
-let lineHTML= ""
-let previousIndex = ""
-let clickSound = new Audio('click.wav')
-let clickSoundTwo = new Audio('click2.wav')
-
+//functions 
 //loops through every element in the selected group and attaches event listeners
-let line = document.querySelectorAll('.list-group-item');
-
 for(i = 0; i < line.length; i++) {
   let currentElement = line[i] 
   line[i].addEventListener('click', function(){
