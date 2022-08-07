85a65eb852ea4cadb24b695bb392e8c1e98d43c6
pfulcher26 <pfulcher26@gmail.com>
2022-08-07 12:15:44 -0700

    Adds a foreach loop to each element of the list-group to attach event listeners to those elements and make them clickable.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ arrowIcon.addEventListener('click', ()=> {
   let instructions = document.querySelector('.instructions');
   instructions.style.visibility !== "hidden" ? instructions.style.visibility = "hidden" : instructions.style.visibility = "visible"; 
 });
+
+let line = document.querySelectorAll('.list-group-item');
+line.forEach((element) => {
+  element.addEventListener('click', function(){
+    console.log('click')
+  })
+});
+
