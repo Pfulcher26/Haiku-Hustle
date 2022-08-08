031e4f4b3332411b93a7755c34fdce9f3c86d5c5
pfulcher26 <pfulcher26@gmail.com>
2022-08-08 10:05:30 -0700

    Adds a shuffle function and calls that function on the haikuArray  to shuffle the order of the poems.  Adds a function expression that shuffles the inner arrays of the shuffled array, thereby randomizing the poems, as well as the order in which their lines appear.

--- js/main.js
+++ js/main.js
@@ -180,3 +180,27 @@ function unfade(element) {
 //establish a way to select the content of the line 
 // console.log(line[0].innerHTML)
 
+
+
+//Array shuffle function 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+let shuffledArray = shuffle(haikuArray) 
+let shuffledInnerArrays = shuffledArray.map(array => shuffle(array))
+
